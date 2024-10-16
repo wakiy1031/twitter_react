6b999564015f6cdc1f25b3712ee7f8d32e0f83aa
wakiy1031 <wakiy1031@gmail.com>
2024-10-16 22:33:36 +0900

    post オブジェクトを分割代入

--- src/features/post/components/postItem.jsx
+++ src/features/post/components/postItem.jsx
@@ -2,19 +2,21 @@ import { Box, Text, Flex, Avatar } from "@yamada-ui/react";
 import { PostImages } from "./postImages";
 
 export const PostItem = ({ post }) => {
+  const { content, user, created_at } = post;
+
   return (
     <Box borderBottom="1px solid #dcdcde" py={2} px={3}>
       <Flex alignItems="start">
         <Avatar size="sm" mr={2} />
         <Box>
           <Text>
-            <span className="font-bold">{post.user.name}</span>
+            <span className="font-bold">{user.name}</span>
             <span className="text-gray-500 ml-1 text-sm">
-              @{post.user.username ? post.user.username : post.user.name}
+              @{user.username ? user.username : user.name}
             </span>
-            <span className="text-gray-500 text-sm">・{post.created_at}</span>
+            <span className="text-gray-500 text-sm">・{created_at}</span>
           </Text>
-          <Text mb={2}>{post.content}</Text>
+          <Text mb={2}>{content}</Text>
           <PostImages post={post} />
         </Box>
       </Flex>
