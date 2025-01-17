d06c7ba605320bf5a8b419c1ba82f591e227b5d2
wakiy1031 <wakiy1031@gmail.com>
2025-01-18 07:10:35 +0900

    リポストする際に確認メニューを追加

--- src/components/repostButton.jsx
+++ src/components/repostButton.jsx
@@ -1,5 +1,14 @@
 import { RepeatIcon } from "@yamada-ui/lucide";
-import { IconButton, Tooltip, Text, Box } from "@yamada-ui/react";
+import {
+  IconButton,
+  Tooltip,
+  Text,
+  Box,
+  MenuList,
+  Menu,
+  MenuItem,
+  MenuButton,
+} from "@yamada-ui/react";
 import useSWR from "swr";
 import { fetchPost } from "../features/api/postApi";
 import { useRepost } from "../hooks/useRepost";
@@ -36,41 +45,58 @@ export const RepostButton = ({ post }) => {
   };
 
   return (
-    <Tooltip label="リポスト" openDelay={500} gutter={2} fontSize="xs">
-      <Box
-        position="relative"
-        cursor="pointer"
-        _hover={{
-          "& > .repost-count": {
-            color: "green.500",
-          },
-        }}
-        onClick={handleRepostClick}
-      >
-        <IconButton
-          variant="ghost"
-          icon={<RepeatIcon w={20} />}
-          aria-label="リポスト"
-          size="sm"
-          w={6}
-          p={1}
-          borderRadius="full"
-          color={isReposted ? "green.500" : "gray.200"}
-          _hover={{ bg: "green.50", color: "green.500" }}
-        />
-        <Text
-          fontSize="sm"
-          color={isReposted ? "green.500" : "gray.300"}
-          className="repost-count"
-          animation="slideInUp"
-          position="absolute"
-          top="50%"
-          right="-.25em"
-          transform="translateY(-50%)"
+    <Box position="relative">
+      <Menu>
+        <Tooltip label="リポスト" openDelay={500} gutter={2} fontSize="xs">
+          <Box
+            position="relative"
+            cursor="pointer"
+            _hover={{
+              "& > .repost-count": {
+                color: "green.500",
+              },
+            }}
+          >
+            <MenuButton
+              as={IconButton}
+              variant="ghost"
+              icon={<RepeatIcon w={20} />}
+              aria-label="リポスト"
+              size="sm"
+              w={6}
+              p={1}
+              borderRadius="full"
+              color={isReposted ? "green.500" : "gray.200"}
+              _hover={{ bg: "green.50", color: "green.500" }}
+              onClick={(e) => e.stopPropagation()}
+            />
+            <Text
+              fontSize="sm"
+              color={isReposted ? "green.500" : "gray.300"}
+              className="repost-count"
+              animation="slideInUp"
+              position="absolute"
+              top="50%"
+              right="-.25em"
+              transform="translateY(-50%)"
+            >
+              {repostsCount > 0 ? repostsCount : ""}
+            </Text>
+          </Box>
+        </Tooltip>
+        <MenuList
+          onClick={(e) => e.stopPropagation()}
+          bg="white"
+          border="none"
+          size="xs"
         >
-          {repostsCount > 0 ? repostsCount : ""}
-        </Text>
-      </Box>
-    </Tooltip>
+          {isReposted ? (
+            <MenuItem onClick={handleRepostClick}>リポストを取り消す</MenuItem>
+          ) : (
+            <MenuItem onClick={handleRepostClick}>リポスト</MenuItem>
+          )}
+        </MenuList>
+      </Menu>
+    </Box>
   );
 };
